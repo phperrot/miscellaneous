2d586044af36fe2f418c5b3c8d5531acfa390fa0
phperrot <58410108+phperrot@users.noreply.github.com>
2020-03-05 11:32:32 +0100

    Add files via upload

--- save.sh
+++ save.sh
@@ -1,8 +1,7 @@
 declare -a expressions=('😀' '☺️'' '🧜‍♂️'' '🧚‍♀️' '👓' '🦆' '🦄' '🐌' '🦍' '🦓' '🦛' '🦒' '🦘' '🐃' '🦌' '🦃' '🦩' '🦦' '🐚')
 index=$( jot -r 1  0 $((${#expressions[@]} - 1)) )
 selected_expression=${expressions[index]}
-echo $selected_expression
-
 git add .
 git commit -m "$selected_expression $1 $selected_expression"
 git push
+echo "$selected_expression $1 $selected_expression"
